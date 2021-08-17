03078e39020c8bebda97d28b77b69546c0cae025
bright-blade <88801490+bright-blade@users.noreply.github.com>
2021-08-17 17:40:54 +0300

    this is more update
    i added a label

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 window.onload = function(){
     console.log("hii")
 }
@@ -17,3 +18,26 @@ window.onload = function(){
     }
 
 
+=======
+window.onload = function(){
+    console.log("hii")
+}
+    function addNewCard(){
+        document.querySelector("#id_label_sec").classList.remove("oneCard");
+        document.querySelector("#id_added").classList.remove("oneCard");
+        document.querySelector("#id_label_custom2").classList.remove("oneCard");
+        document.querySelector("#id_add_card").classList.add("oneCard");
+        document.querySelector("#id_one_or_two").innerHTML = "2";
+        
+    }
+    function deleteNewCard(){
+        document.querySelector("#id_label_sec").classList.add("oneCard");
+        document.querySelector("#id_added").classList.add("oneCard");
+        document.querySelector("#id_label_custom2").classList.add("oneCard");
+        document.querySelector("#id_add_card").classList.remove("oneCard");
+        document.querySelector("#id_one_or_two").innerHTML = "1";
+       
+    }
+
+
+>>>>>>> Stashed changes
